e221d8b55b25578effbc97e115d69e67eb0553d4
Rezwanur Rahman <rezwan.cse09@gmail.com>
2015-06-15 19:03:40 +0600

    no message

--- greetings/index.php
+++ greetings/index.php
@@ -15,15 +15,15 @@ $greet_msg=$str_arr['0'];
 
 	if($greet_msg=="Hello"){
 		$reply="Hello, Kitty!"."I am fine! How are you?";
-		echo $reply;
+
 	}
 	elseif($greet_msg=="Hi"){
 		$reply="Hello, Kitty!"."My name is Rezwan";
-		echo $reply;
+		
 	}
 	elseif($greet_msg=="Good morning"||$greet_msg=="Good evening"||$greet_msg=="Good night"){
 		$reply="Hello, Kitty!"."It’s a pleasure to meet you!";
-		echo $reply;
+		
 	}
 	else{
 		$reply="Hello, Kitty!"."I don't understand what you want to say,but,I am very happy to meet you!";
